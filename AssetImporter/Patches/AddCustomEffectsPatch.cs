ea790a8023f03f46f58ed8ae3bd48eb05a08fd9c
Nympfonic <arys.steam@gmail.com>
2024-02-03 20:10:44 +0000

    Fixed the asset loader failing if the bundles are placed in subfolders within the effects directory

--- AssetImporter/Patches/AddCustomEffectsPatch.cs
+++ AssetImporter/Patches/AddCustomEffectsPatch.cs
@@ -3,6 +3,7 @@ using CustomAssetImporter.Templates;
 using CustomAssetImporter.Util;
 using HarmonyLib;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using Systems.Effects;
@@ -22,32 +23,34 @@ namespace CustomAssetImporter.Patches
         private static void PatchPrefix(Effects __instance)
         {
             string directory = "assets\\effects\\";
-            string[] effectsBundles = AssetLoader.ReadBundleNamesFromDirectory(directory);
+            string[] effectsBundles = AssetLoader.GetBundlePathsFromDirectory(directory);
             if (!effectsBundles.Any())
             {
                 Plugin.LogSource.LogWarning($"\"{directory}\" does not contain any bundles. No custom effects will be added.");
                 return;
             }
 
-            AddCustomEffects(__instance, directory, effectsBundles);
+            AddCustomEffects(__instance, effectsBundles);
         }
 
-        private static void AddCustomEffects(Effects effects, string directory, string[] effectsBundles)
+        private static void AddCustomEffects(Effects effects, string[] effectsBundles)
         {
             List<Effects.Effect> customEffectsList = [];
-            foreach (string bundle in effectsBundles)
+            foreach (string bundlePath in effectsBundles)
             {
-                GameObject gameObject = AssetLoader.LoadAsset($"{directory}{bundle}");
+                string bundleName = Path.GetFileName(bundlePath);
+                string bundleRelativePath = AssetLoader.GetRelativePath(Plugin.Directory, bundlePath);
+                GameObject gameObject = AssetLoader.LoadAsset(bundleRelativePath);
                 GameObject instantiatedEffects = Object.Instantiate(gameObject);
                 if (!instantiatedEffects.TryGetComponent<CustomEffectsTemplate>(out var customEffects))
                 {
-                    Plugin.LogSource.LogError($"\"{bundle}\" does not contain a valid CustomEffects script. Skipping...");
+                    Plugin.LogSource.LogError($"\"{bundleName}\" does not contain a valid CustomEffects script. Skipping...");
                     continue;
                 }
 
                 if (!customEffects.EffectsArray.Any())
                 {
-                    Plugin.LogSource.LogError($"\"{bundle}\" CustomEffects script has an empty EffectsArray. Skipping...");
+                    Plugin.LogSource.LogError($"\"{bundleName}\" CustomEffects script has an empty EffectsArray. Skipping...");
                     continue;
                 }
 
@@ -56,13 +59,13 @@ namespace CustomAssetImporter.Patches
                     bool hasDuplicates = FrameworkUtil.CheckForDuplicates(customEffectsList, customEffects.EffectsArray, new EffectComparer());
                     if (hasDuplicates)
                     {
-                        Plugin.LogSource.LogError($"\"{bundle}\" EffectsArray has an Effect with a conflicting Name property with an existing custom Effect Name. Skipping...");
+                        Plugin.LogSource.LogError($"\"{bundleName}\" EffectsArray has an Effect with a conflicting Name property with an existing custom Effect Name. Skipping...");
                         continue;
                     }
                 }
 
                 customEffectsList.AddRange(customEffects.EffectsArray);
-                Plugin.LogSource.LogInfo($"\"{bundle}\"'s effects have been added.");
+                Plugin.LogSource.LogInfo($"\"{bundleName}\"'s effects have been added.");
 
                 foreach (var child in instantiatedEffects.transform.GetChildren())
                 {
